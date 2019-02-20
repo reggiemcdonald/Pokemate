4f92a4a2189621f1b7975e404f770004ca516efa
i5g2b <i5g2b@ugrad.cs.ubc.ca>
2019-02-19 23:58:19 -0800

    Added character view

--- App.js
+++ App.js
@@ -11,6 +11,7 @@ import React, {Component} from 'react';
 import {Platform, SectionList, StyleSheet, Text, View} from 'react-native';
 import PokemonMainList from "./src/components/PokemonMainList";
 import PokeDataProcessor from "./src/library/networking/PokeDataProcessor";
+import PokemonCharacterView from "./src/components/PokemonCharacterView";
 //
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -25,7 +26,6 @@ export default class App extends Component<Props> {
     super();
     this.state = {
       processor: new PokeDataProcessor(),
-      data: []
     };
   }
 
@@ -34,56 +34,13 @@ export default class App extends Component<Props> {
     console.log("rendering");
     return (
         <View style={styles.container}>
-          <PokemonMainList processor = {this.state.processor}/>
+          {/*<PokemonMainList processor = {this.state.processor}/>*/}
+          <PokemonCharacterView/>
         </View>
 
     );
   }
 
-  async componentDidMount(): void {
-    let pokemon = await this.state.processor.getListOfPokemon();
-    pokemon = this.makeListWithSectionHeaders(pokemon);
-    this.setState({
-      data: pokemon
-    });
-  }
-
-  makeListWithSectionHeaders(list) {
-    console.log("Making");
-    let objectWithHeaders = {};
-    for (let item of list) {
-      let header = item.substr(0,1).toUpperCase();
-      if (objectWithHeaders.hasOwnProperty(header)) {
-        let arr = objectWithHeaders[header];
-        arr.push(item);
-        objectWithHeaders[header] = arr;
-      } else {
-        objectWithHeaders[header] = [item];
-      }
-    }
-    let listWithHeaders = [];
-    let headers = Object.keys(objectWithHeaders);
-    for (header of headers) {
-      let obj = {
-        header: header,
-        data: objectWithHeaders[header]
-      };
-      listWithHeaders.push(obj);
-    }
-    return this.sortListOnHeaders(listWithHeaders);
-  }
-
-  sortListOnHeaders(list) {
-    return list.sort((a,  b) => {
-      if (a.header > b.header) {
-        return 1;
-      } else if (a.header < b.header) {
-        return -1;
-      } else {
-        return 0;
-      }
-    });
-  }
 }
 
 const styles = StyleSheet.create({
---
+++ src/components/PokemonCharacterView.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import {View, Text, Image, StyleSheet, FlatList} from 'react-native';
+/**
+ * ************************
+ * Detailed view of pokemon
+ * ************************
+ * Takes in a prop of the pokemon data processor
+ */
+export default class PokemonCharacterView extends React.Component{
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+           <View style={styles.container}>
+               <Text style={[styles.genericText, styles.titleText]}>Pokemon Title</Text>
+               <View style={styles.spriteContainer}>
+                   <Image source={{uri: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png"}}
+                          style={styles.sprite}/>
+               </View>
+               <Text style={styles.headerText}>Type</Text>
+               <Text style={[styles.genericText, styles.subText]}>Ditto is a normal type!</Text>
+               <Text style={styles.headerText}>Types Ditto is Strong Against</Text>
+               <Text style={styles.subText}>Such empty...</Text>
+               <Text style={styles.headerText}>Types Ditto is Weak Against</Text>
+               <FlatList data={[
+                   {key: "Fighting"}
+                   ]}
+                         renderItem={({item}) => <Text style={styles.subText}>{item.key}</Text>}
+               />
+               <Text style={styles.headerText}>Types that have no effect on Ditto</Text>
+               <FlatList data={[{key: "Ghost"}]}
+                         renderItem={({item}) => <Text style={styles.subText}>{item.key}</Text>}
+               />
+           </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: "#F5FCFF",
+        paddingTop: 50,
+        flex: 1,
+    },
+    titleText: {
+        fontSize: 30,
+        fontWeight: "bold",
+        paddingLeft: 10
+    },
+    headerText: {
+        fontSize: 20,
+        fontWeight: "bold",
+        paddingLeft: 10
+    },
+    subText: {
+        fontSize: 18,
+        paddingLeft: 10
+    },
+    spriteContainer:{
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    sprite: {
+        width: 300,
+        height: 300,
+    },
+});
